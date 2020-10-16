9b74bad0142fb0ed0776f35167475ccc43b01349
unknown <guilherme.cano@outlook.com>
2020-10-16 14:42:12 -0300

    implementacao de security no projeto

--- src/main/java/com/longevida/repository/UsuarioRepository.java
+++ src/main/java/com/longevida/repository/UsuarioRepository.java
@@ -1,9 +1,5 @@
 package com.longevida.repository;
 
-<<<<<<< HEAD
-=======
-import java.util.List;
->>>>>>> 83e8185965ee2dfc942fa7bea2a391ccffdf826b
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,17 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.longevida.model.Usuario;
 
 
-<<<<<<< HEAD
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
 	public Optional<Usuario> findByUsuario(String usuario);
 	
 	
-=======
-	public Optional<Usuario> findByLoginUsuario(String loginUsuario);
-
-	
-	
-
->>>>>>> 83e8185965ee2dfc942fa7bea2a391ccffdf826b
 }
--- src/main/java/com/longevida/service/UsuarioService.java
+++ src/main/java/com/longevida/service/UsuarioService.java
@@ -12,39 +12,25 @@ import com.longevida.model.UserLogin;
 import com.longevida.model.Usuario;
 import com.longevida.repository.UsuarioRepository;
 
-<<<<<<< HEAD
 
 
 @Service
 public class UsuarioService {
 
-=======
-@Service
-public class UsuarioService {
->>>>>>> 83e8185965ee2dfc942fa7bea2a391ccffdf826b
 	@Autowired
 	private UsuarioRepository repository;
 	
 	public Usuario CadastrarUsuario(Usuario usuario) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
-<<<<<<< HEAD
 		String senhaEncoder = encoder.encode(usuario.getSenha());
 		usuario.setSenha(senhaEncoder);
 		
 		return repository.save(usuario);	
-=======
-		
-		String senhaEnconder = encoder.encode(usuario.getSenhaUsuario());
-		usuario.setSenhaUsuario(senhaEnconder);
-		
-		return repository.save(usuario);
->>>>>>> 83e8185965ee2dfc942fa7bea2a391ccffdf826b
 	}
 	
 	public Optional<UserLogin> Logar(Optional<UserLogin> user){
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-<<<<<<< HEAD
 		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
 		
 		if(usuario.isPresent()) {
@@ -67,25 +53,3 @@ public class UsuarioService {
 		
 }
 
-=======
-		Optional<Usuario>usuario = repository.findByLoginUsuario(user.get().getUsuario());
-		
-		if(usuario.isPresent()) 
-		{
-			if(encoder.matches(user.get().getSenha(), usuario.get().getSenhaUsuario()))  
-			{
-				String auth = user.get().getUsuario() + ":"+ user.get().getSenha();
-				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-				String authHeader = "Basic " + new String(encodeAuth); 
-				
-				user.get().setToken(authHeader);
-				user.get().setNome(usuario.get().getNomeUsuario());
-				
-				
-				return user;
-			}
-		}
-		return null;
-	}
-}
->>>>>>> 83e8185965ee2dfc942fa7bea2a391ccffdf826b
