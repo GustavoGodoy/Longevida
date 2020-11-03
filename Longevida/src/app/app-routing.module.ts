25b4f57dbf8fe0be5f23a0608c0a1943654008b0
Gustavo Godoy <gustavocoliver@gmail.com>
2020-11-03 16:27:39 -0300

    Longevida Angular Project

--- Longevida/src/app/app-routing.module.ts
+++ Longevida/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', redirectTo: 'home',pathMatch:'full'}, 
+{path:'home', component: HomeComponent},
+{path:'produtos', component: ProdutosComponent},
+{path:'contato', component: ContatoComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
